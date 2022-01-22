d06774c50ae415c0f5b3c8587375f420ceb30c68
virginie <virginie.villard@le-campus-numerique.fr>
2022-01-22 22:07:45 +0100

    Démarrage du container Docker avec Apache, phpmyadmin

---
+++ README.md
@@ -0,0 +1,13 @@
+# Welcome in my Docker Container #
+
+## Commands to use container ##
+
+### Up (launch) ###
+```bash
+docker-compose up -d
+```
+### Down (stop) ###
+```bash
+docker-compose down
+```
+
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// https://dev.to/truthseekers/setup-a-basic-local-php-development-environment-in-docker-kod
+// https://hub.docker.com/_/phpmyadmin  (pour installer phpmyadmin)
+
+echo "Hello from Docker, I'm so happy !";
+
