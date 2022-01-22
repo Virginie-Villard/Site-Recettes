e274954d0bf49d45202b823addf78d5f6f6ab3f1
virginie <virginie.villard@le-campus-numerique.fr>
2022-01-23 00:24:57 +0100

    DB persistante

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// https://dev.to/truthseekers/setup-a-basic-local-php-development-environment-in-docker-kod
+// https://dev.to/truthseekers/setup-a-basic-local-php-development-environment-in-docker-kod  (Mise en place du container Docker)
 // https://hub.docker.com/_/phpmyadmin  (pour installer phpmyadmin)
 
-echo "Hello from Docker, I'm so happy !";
+
 
