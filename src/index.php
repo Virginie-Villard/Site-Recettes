b00f1542fde768a6b963e59011507e83a7bcb5f0
virginie <virginie.villard@le-campus-numerique.fr>
2022-01-23 00:37:25 +0100

    Ajout PDO au Dockerfile.

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
+<h1>Recettes !</h1>
 <?php
 
 // https://dev.to/truthseekers/setup-a-basic-local-php-development-environment-in-docker-kod  (Mise en place du container Docker)
 // https://hub.docker.com/_/phpmyadmin  (pour installer phpmyadmin)
 
-
-
+$pdo = new PDO('mysql:host=db;dbname=recipes;charset=utf8', 'root', 'password');
