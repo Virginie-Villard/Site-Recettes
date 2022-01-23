5dfd0e4a5f9c3ff67f321e7166c644e759073858
virginie <virginie.villard@le-campus-numerique.fr>
2022-01-23 01:04:36 +0100

    Affichage et test des requetes

--- src/index.php
+++ src/index.php
@@ -4,4 +4,34 @@
 // https://dev.to/truthseekers/setup-a-basic-local-php-development-environment-in-docker-kod  (Mise en place du container Docker)
 // https://hub.docker.com/_/phpmyadmin  (pour installer phpmyadmin)
 
-$pdo = new PDO('mysql:host=db;dbname=recipes;charset=utf8', 'root', 'password');
+try
+{
+    $pdo = new PDO('mysql:host=db;dbname=recipes;charset=utf8',
+        'root',
+        'password',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+// On récupère tout le contenu de la table recipes
+$query = 'SELECT * FROM recipes';
+$statement = $pdo->prepare($query);
+$statement->execute();
+$recipes = $statement->fetchAll();
+
+// Fonction qui affiche le nom d'une recette
+function displayRecipe($recipe)
+{
+    echo "<h2>" . $recipe["title"] . "</h2>";
+    echo "<p>" . $recipe["description"] . "</p>";
+}
+
+// On affiche chaque recette une à une
+foreach ($recipes as $recipe)
+{
+    displayRecipe($recipe);
+}
+?>
