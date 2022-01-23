48dc4d996de9cda924bb8f764914a483492aa16a
virginie <virginie.villard@le-campus-numerique.fr>
2022-01-23 15:41:36 +0100

    Création formulaire d'inscription

---
+++ src/register.php
@@ -0,0 +1,17 @@
+<!-- Formulaire d'inscription avec email password et bouton -->
+
+<form method="post" action="submit_form.php">
+
+    <label for="user_name">User name:</label><br>
+    <input id="user_name" type="text" name="user_name"/><br>
+
+    <label for="email">e-mail:</label><br>
+    <input id="email" type="email" name="email"/><br>
+
+    <label for="password">Password:</label><br>
+    <input id="password" type="password" name="password"/><br>
+
+    <input type="submit" name="submit" value="Valider"/><br>
+
+</form>
+
